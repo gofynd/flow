2e048b8b9c4e74425a94b95b502181e212b9ace8
Jigar Dafda <jigar.dafda@gmail.com>
2024-04-05 11:18:55 +0000

    [Auto Generated]

---
+++ theme/components/blog/BlogPage.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useParams } from 'react-router-dom';
+import EmptyState from '../../page-layouts/marketing/EmptyState';
+import { useGlobalStore ,useFPI} from "fdk-core/utils";
+
+function BlogPage() {
+	const fpi = useFPI();
+	const { slug } = useParams();
+
+	const blog = useGlobalStore(fpi.getters.BLOG) || {};
+
+	useEffect(() => {
+		if (blog?.slug !== slug) {
+			fpi.content.fetchBlogData({ slug });
+		}
+	}, [slug]);
+
+	const {
+		seo = {},
+		content = [],
+		loading,
+		error,
+	} = blog;
+
+	const { title, description } = seo;
+
+	const pageNotFound = error?.name === 'FDKServerResponseError' && error?.message === 'Sorry, document not found';
+
+	const htmlContent = content.find((data) => (data.type === 'rawhtml' || data.type === 'html'))?.value ?? '';
+
+	const Content = pageNotFound ? <EmptyState message="Blog not found!" />
+		: (
+			<div id="custom-page-container" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+		);
+
+	if (loading) {
+		return <h2>Loading Blog content</h2>;
+	}
+
+	return (
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>{title}</title>
+				<meta name="description" content={description} />
+			</Helmet>
+			{Content}
+		</>
+	);
+}
+
+BlogPage.serverFetch = ({ router, fpi }) => {
+	const { slug } = router?.params ?? {};
+	return fpi.content.fetchBlogData({ slug });
+};
+
+export default BlogPage;
---
+++ theme/pages/home.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from "react";
+import { SectionRenderer } from "fdk-core/components";
+import { useGlobalStore } from "fdk-core/utils";
+import Loader from "../components/loader/loader";
+import { getThemeGlobalConfig } from "../helper/theme";
+
+
+function Home({ numberOfSections, fpi }) {
+  const page = useGlobalStore(fpi.getters.PAGE) || {};
+  const THEME = useGlobalStore(fpi.getters.THEME);
+  const globalConfig = getThemeGlobalConfig(THEME?.config);
+  const { sections = [], loading, error } = page || {};
+  useEffect(() => {
+    fpi.collection.fetchCollection({}).then((e) => console.log(e));
+  }, []);
+
+  if (error) {
+    return (
+      <>
+        <h1>Error Occured !</h1>
+        <pre>{JSON.stringify(error, null, 4)}</pre>
+      </>
+    );
+  }
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <div className="wrapper">
+      <SectionRenderer
+        sections={sections}
+        fpi={fpi}
+        globalConfig={globalConfig}
+      />
+    </div>
+  );
+}
+
+export const settings = JSON.stringify({
+  props: [
+    {
+      id: "code",
+      label: "Your Code Here",
+      type: "code",
+      default: "",
+      info: "Add Your custom HTML Code below. You can also use the full screen icon to open a code editor and add your code",
+    },
+    {
+      type: "extension",
+      id: "extension",
+      label: "Extension Positions",
+      info: "Handle extension in these positions",
+      positions: [
+        {
+          value: "below_price_component",
+          text: "Below Price Component",
+        },
+        {
+          value: "above_price_breakup",
+          text: "Above Price Breakup",
+        },
+        {
+          value: "below_product_info",
+          text: "Below Delivery location",
+        },
+        {
+          value: "product_description_bottom",
+          text: "Below Product Description",
+        },
+      ],
+      default: {},
+    },
+  ],
+  blocks: [],
+});
+
+export const sections = JSON.stringify([
+  {
+    attributes: {
+      page: "home",
+    },
+  },
+]);
+
+Home.serverFetch = ({ fpi }) => fpi.content.fetchNavigation();
+
+export default Home;
